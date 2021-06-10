13e432fac8435f0e7dda42ba81438b2ae057eab5
ikhsantasef <ifikri72@gmail.com>
2021-06-10 12:38:37 +0700

    benerin dikit

--- index.php
+++ index.php
@@ -59,9 +59,9 @@ include_once "function/rupiah.php";
 
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div class="col-md-5 p-lg-5 mx-auto my-5">
-      <h1 class="display-4 font-weight-normal">Punny headline</h1>
-      <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-      <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+      <h1 class="display-4 font-weight-normal">Yuk Belanja di YUKCetak</h1>
+      <p class="lead font-weight-normal">Dapatkan Harga Spesial dengan Berbelanja Rutin di YUKCetak Store</p>
+   
     </div>
     <div class="product-device shadow-sm d-none d-md-block"></div>
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
